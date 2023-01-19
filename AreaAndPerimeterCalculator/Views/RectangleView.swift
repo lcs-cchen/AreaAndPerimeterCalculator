4349e1cf62f615522acd9cf172c944a3aca03ba3
cyrus chen <pschen@lcs.on.ca>
2023-01-19 11:17:09 -0500

    set up an empty view to calculate the area and premiter of a rectangle

---
+++ AreaAndPerimeterCalculator/Views/RectangleView.swift
@@ -0,0 +1,20 @@
+//
+//  RectangleView.swift
+//  AreaAndPerimeterCalculator
+//
+//  Created by Cyrus Chen on 19/1/2023.
+//
+
+import SwiftUI
+
+struct RectangleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RectangleView_Previews: PreviewProvider {
+    static var previews: some View {
+        RectangleView()
+    }
+}
